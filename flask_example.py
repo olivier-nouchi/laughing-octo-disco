6a511c0b40ea66865a6529aba7798ddb29d1e97f
Olivier NOUCHI <olivier.nouchi@gmail.com>
2021-07-09 12:56:09 +0300

    nice

--- flask_example.py
+++ flask_example.py
@@ -6,7 +6,7 @@ example_app = Flask(__name__)
 
 @example_app.route('/')
 def hello_world():
-    return 'Hello Fellows February 2020. You rock, this is fun!'
+    return 'Hello Fellows DS Evening 2020. I wish you a great weekend!'
 
 
 if __name__ == '__main__':
